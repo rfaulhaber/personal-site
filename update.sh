33762446e046c6a86e68a06045d1924e80b90022
Ryan Faulhaber <faulhaber.14@osu.edu>
2019-03-18 08:06:01 -0400

    add update content script

---
+++ update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# shamelessly stolen from the Hugo site, with some modifications
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo
+
+cd public
+git add --all
+
+# Commit changes.
+
+msg="content updated `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back up to the Project Root
+cd ..
